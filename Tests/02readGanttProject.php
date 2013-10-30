870468d5446865eb9bc65610398379f78347d9d1
Tuomas Angervuori <tuomas.angervuori@gmail.com>
2013-10-30 21:36:44 +0200

    Fixed typo

--- Tests/02readGanttProject.php
+++ Tests/02readGanttProject.php
@@ -4,7 +4,7 @@
 	error_reporting(E_ALL);
 
 	/** Include path **/
-	ini_set('include_path', ini_get('include_path').';../Classes/');
+	ini_set('include_path', ini_get('include_path').':../Classes/');
 
 	/** PHPProject */
 	include 'PHPProject.php';
